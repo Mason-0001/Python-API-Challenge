9c930b8a46a989b8735c2b2e74765fbeff62b2f5
Mason-001 <119235096+Mason-0001@users.noreply.github.com>
2023-01-29 23:07:13 -0500

    adding completed files

---
+++ .gitignore/api_keys.py
@@ -0,0 +1,8 @@
+# Adding config.py file.
+#api_keys.py
+
+# OpenWeatherMap API Key
+weather_api_key = "01ca1365869510f5833e25fe37b8ef29"
+
+# Geoapify API Key
+geoapify_key = "fcbd68dd6c5049739bbbdebdc59d89c9"
